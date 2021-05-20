fd98876d57e877b202f015d72833f1a0cba3ecda
sonia <s.rolland@codeur.online>
2021-05-20 15:51:57 +0200

    udapte

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paresseux World</title>
+      <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header><h1 id="title">Le monde des Paresseux</h1></header>
+<div class="container">
+
+  <div class="row">
+    <div class="col-sm">
+    <div class="card" style="width: 18rem;">
+  <img src="img/branche.gif" class="card-img-top card-img-vh un" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Les paresseux</h5>
+    <p class="card-text">Apprenez-en plus!</p>
+    <a href="https://fr.wikipedia.org/wiki/Paresseux" class="btn btn-brown" target="_blank">A propos des paresseux</a>
+  </div>
+</div>
+    </div>
+</body>
+</html>
