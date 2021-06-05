9cb0101368153d6a48c5d9a388bedd5000bcc3d7
Rolland42 <dianla2@hotmail.fr>
2021-06-05 10:45:13 +0200

    test

--- index.php
+++ index.php
@@ -21,13 +21,13 @@
         <ul id="menu">
           <li><a href="#">Alimentation/sommeil </a></li>
           <li><a href="#">Biologie/Accouplement</a></li>
-          <li><a href="#">Habitat/Chasser !!</a></li>
+          <li><a href="#">Habitat/Prédateurs</a></li>
           <li><a href="#">Crédits</a></li>
         </ul>
       <div style="width: 200px; height: 1000px"></div>
      
     
-  </div>
+ 
      <div class="leaf">
      <div>  <img src="http://www.pngmart.com/files/1/Green-Leaves-PNG-File.png" height="75px" width="75px"></img></div>
       <div><img src="http://www.pngmart.com/files/1/Green-Leaves-PNG-File.png" height="75px" width="75px"></img></div>
